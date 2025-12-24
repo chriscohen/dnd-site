da036b3d021958e6d5a6742f2399735f5ea770da
Chris Cohen <malone82@gmail.com>
2025-12-24 15:28:59 +0000

    Added mapping table for status condition immunities.

---
+++ database/migrations/create_status_condition_immunities_table.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use App\Models\Creatures\CreatureEdition;
+use App\Models\StatusConditions\StatusConditionEdition;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('status_condition_immunities', function (Blueprint $table) {
+            $table->foreignIdFor(StatusConditionEdition::class, 'status_condition_edition_id');
+            $table->foreignIdFor(CreatureEdition::class, 'creature_edition_id');
+            $table->primary(['status_condition_edition_id', 'creature_edition_id'], 'status_condition_creature');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('status_condition_immunities');
+    }
+};
