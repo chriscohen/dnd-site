96e71606bb8aa71ee7f985dbcd612e3c865598c0
Chris Cohen <malone82@gmail.com>
2025-12-24 15:28:39 +0000

    Added get() to CreatureController.

--- app/Http/Controllers/Creatures/CreatureController.php
+++ app/Http/Controllers/Creatures/CreatureController.php
@@ -4,15 +4,34 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Creatures;
 
+use App\DTOs\Creatures\CreatureFullDTO;
+use App\DTOs\Creatures\CreatureSummaryDTO;
 use App\Http\Controllers\AbstractController;
 use App\Http\Requests\CreatureListRequest;
 use App\Models\Creatures\Creature;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class CreatureController extends AbstractController
 {
     protected string $entityType = Creature::class;
     protected string $orderKey = 'name';
+
+    public function get(Request $request, string $slug): JsonResponse
+    {
+        $this->preValidate($request);
+
+        /** @var Creature|null $item */
+        $item = $this->query
+            ->where('slug', $slug)
+            ->with([
+                'editions'
+            ])
+            ->first();
+        $item = CreatureFullDTO::fromModel($item);
+
+        return response()->json($item ?? []);
+    }
 
     public function list(CreatureListRequest $request): JsonResponse
     {
@@ -24,12 +43,8 @@ class CreatureController extends AbstractController
 
         $this->query->orderBy($this->orderKey);
 
-        $items = $this->query->get();
+        $items = $this->query->paginate(20)->through(fn(Creature $item) => CreatureSummaryDTO::fromModel($item));
 
-        foreach ($items as $item) {
-            $output[] = $item->toArray($this->getMode($request));
-        }
-
-        return response()->json($output ?? []);
+        return response()->json($items);
     }
 }
