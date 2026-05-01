3da62412bd4cdffdcd974def084ef91cb5d22690
Chris Cohen <malone82@gmail.com>
2026-05-01 02:25:26 +0000

    Fix conflicting CSS selectors causing certain form elements to use the light theme even when dark mode is in use.

--- admin/src/main.ts
+++ admin/src/main.ts
@@ -2,6 +2,7 @@ import './assets/main.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import Aura from '@primeuix/themes/aura';
 
 import App from './App.vue';
 import router from './router';
@@ -11,6 +12,13 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(PrimeVue);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: '.p-dark',
+        },
+    },
+});
 
 app.mount('#app');
